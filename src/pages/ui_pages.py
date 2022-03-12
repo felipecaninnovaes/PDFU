0ae9c5e8d2f7008f255bfd337b75efce7765516d
FelipeCN <57776624+felipecaninnovaes@users.noreply.github.com>
2022-03-12 17:12:58 -0300

    Bump version

--- src/pages/ui_pages.py
+++ src/pages/ui_pages.py
@@ -8,6 +8,22 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
+#///////////////////////////////////////////////////////////////
+#
+# BY: FELIPE CANIN NOVAES
+# PROJECT MADE WITH: PDFU
+# V: 1.0.3
+#
+# This project can be used freely for all uses, as long as they maintain the
+# respective credits only in the Python scripts, any information in the visual
+# interface (GUI) can be modified without any implication.
+#
+# There are limitations on Qt licenses if you want to use your products
+# commercially, I recommend reading them on the official website:
+# https://doc.qt.io/qtforpython/licenses.html
+#
+# ///////////////////////////////////////////////////////////////
+
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
